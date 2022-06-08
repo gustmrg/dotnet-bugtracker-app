4394d785bfbb72194b8bf3cdc2210e41353e9c29
Gustavo Miranda <gmiranda90@outlook.com>
2022-06-08 11:54:21 -0300

    Created project and started its initial setup.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BugTracker.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace BugTracker.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+    
+    [NotMapped]
+    [Display(Name = "Full Name")]
+    public string FullName => $"{FirstName} {LastName}";
+}
